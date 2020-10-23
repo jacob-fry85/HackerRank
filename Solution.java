9e949ccbe179203562698f3bcbdc0220d6c0a9e5
Darwin Jacob <72287071+jacob-fry85@users.noreply.github.com>
2020-10-23 17:53:35 +0700

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+//Complete this code or write your own from scratch
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Dictionary dictionary = new Hashtable();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            dictionary.put(name, phone);
+        }
+        boolean isfound;
+        while (in.hasNext()) {
+            isfound = false;
+            String s = in.next();
+            if(dictionary.get(s) != null) {
+                System.out.println(s + "=" + dictionary.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
